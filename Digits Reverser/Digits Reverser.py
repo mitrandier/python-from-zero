d49dfa4a36beb8c56be0287ecb18e4eaad0cd539
Mehdi <mehdiporoki@gmail.com>
2026-01-06 09:56:59 +0330

    Update Digits Reverser.py

--- Digits Reverser/Digits Reverser.py
+++ Digits Reverser/Digits Reverser.py
@@ -1,13 +1,17 @@
 a = int(input())
-
+print(a)
 b = a%10
+print(b)
 
 c =(a//10)%10
+print(c)
 
 d = (a//100)%10
+print(d)
 
 e = a//1000
+print(e)
 
 b ,c ,d ,e = str(b) ,str(c) ,str(d) ,str(e)
 
-print( e+b+c+d)
+print(e+b+c+d)
