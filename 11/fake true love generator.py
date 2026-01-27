1dea6499967da4d7d4e3bda68628eb76f97c0132
Mehdi <mehdiporoki@gmail.com>
2026-01-27 19:14:07 +0330

    Add files via upload

---
+++ 11/fake true love generator.py
@@ -0,0 +1,5 @@
+import random
+name1= input("please enter first person name:  ")
+name2= input("please enter second person name:  ")
+true_love = random.randint(1,100)
+print(f"the score of {name1} and {name2} true love is: {true_love}%.")
