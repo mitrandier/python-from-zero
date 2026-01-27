ece4f9c5f673ff3ebdd0d74dc6a62986a2cff326
Mehdi <mehdiporoki@gmail.com>
2026-01-27 19:14:39 +0330

    Add files via upload

---
+++ 12/head or tails.py
@@ -0,0 +1,9 @@
+print(" Hello and wellcome to Head and Tails game")
+import random
+
+random1 = random.randint(0,1)
+if random1 == 1:
+    print("** Heads **")
+else:
+    print("** Tails **")
+    
