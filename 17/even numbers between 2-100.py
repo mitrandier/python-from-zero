7ce04d325a6e9d9af57dbe3309db391f2c7a5dc0
Mehdi <mehdiporoki@gmail.com>
2026-01-29 23:04:33 +0330

    Add files via upload

---
+++ 17/even numbers between 2-100.py
@@ -0,0 +1,12 @@
+num_sum = 0 
+for number in range (2,101,2):
+    num_sum += number
+print(num_sum)
+
+number_sum = 0
+for i in range (2, 101):
+    if i%2 == 0:
+        number_sum += i
+    
+print(number_sum)
+
