a7cbe52635ff2bc3f9e4dfb345ffea0bb0245967
Mehdi <mehdiporoki@gmail.com>
2026-02-03 16:53:46 +0330

    Add files via upload

---
+++ 28/keyword arguments.py
@@ -0,0 +1,8 @@
+def greet_with(name, age):
+    print(f"Hi {name}")
+    print(f"Your age is:{age}")
+
+name = input("Hi enter your name: ")
+age = int(input("enter your age number:"))
+
+print(greet_with(name,age))
