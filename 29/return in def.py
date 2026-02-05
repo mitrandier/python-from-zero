1cb79db6945ac3ce3f8052bb29c464b00e7b0a22
Mehdi <mehdiporoki@gmail.com>
2026-02-05 16:16:50 +0330

    Add files via upload

---
+++ 29/return in def.py
@@ -0,0 +1,7 @@
+def area_triangle(base, height):
+    return (base*height)/2
+
+area_a = area_triangle(5,4)
+area_b = area_triangle(7,3)
+area_total = area_a + area_b
+print(f"The sum of both areas is:{area_total}")
